c2d4a177cd46761a741b89a5e38548c045afd9f2
Rupert Foggo McKay <maxfmckay@gmail.com>
2025-10-31 12:54:26 +0100

    2025 october

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Hosted on [rupertmckay.com/video-diary](https://rupertmckay.com/video-diary)
 
 I use [One Second Diary](https://play.google.com/store/apps/details?id=com.kylekun.one_second_diary&pcampaignid=web_share) to make the videos.
 
-At the end of each month I upload the full video to Youtube, where I keep it as an _unlisted_ video. From there I get the embeddable iframe from the share video modal.
+At the end of each month I upload the full video to Youtube, where I keep it as an _unlisted_ video. From there I get the embeddable source URL from the share video modal.
 
 Aside from managing the video files, only a small HTML update is needed with each upload to present the video in the webpage.
 
--- index.js
+++ index.js
@@ -2,33 +2,64 @@ const template = document.querySelector("#video-diary-entry-template");
 const main = document.querySelector("main");
 
 const render = ({ date, src }) => {
-    // Derived state
-    const year = date.getFullYear();
-    const shortMonth = date.toLocaleString("default", { month: "short" });
-    const longMonth = date.toLocaleString("default", { month: "long" });
-    const title = `${longMonth} ${year}`;
-    const id = `${year}${shortMonth}`;
+  // Derived state
+  const year = date.getFullYear();
+  const shortMonth = date.toLocaleString("default", { month: "short" });
+  const longMonth = date.toLocaleString("default", { month: "long" });
+  const title = `${longMonth} ${year}`;
+  const id = `${year}${shortMonth}`;
 
-    // Clone the template and fill in the details
-    const clone = template.content.cloneNode(true);
-    clone.querySelector("h3").id = id;
-    clone.querySelector("h3 a").href = `#${id}`;
-    clone.querySelector("h3 a").textContent = title;
-    clone.querySelector("iframe").src = src;
-    clone.querySelector("iframe").title = title;
-    main.appendChild(clone);
-};  
+  // Clone the template and fill in the details
+  const clone = template.content.cloneNode(true);
+  clone.querySelector("h3").id = id;
+  clone.querySelector("h3 a").href = `#${id}`;
+  clone.querySelector("h3 a").textContent = title;
+  clone.querySelector("iframe").src = src;
+  clone.querySelector("iframe").title = title;
+  main.appendChild(clone);
+};
 
 const videoDiaryEntries = [
-    { date: new Date("2025-01"), src: "https://www.youtube.com/embed/bKIL70iP5Nc?si=Xc0D-TvtZ0Dx_WU3" },
-    { date: new Date("2025-02"), src: "https://www.youtube.com/embed/lggIn-1yd8Q?si=fmNzsKmeaKYTCAxQ" },
-    { date: new Date("2025-03"), src: "https://www.youtube.com/embed/1tHzphoNXXM?si=Qzx7iRUMz7pKr0_-" },
-    { date: new Date("2025-04"), src: "https://www.youtube.com/embed/sTchSm6BqQQ?si=FoASemMCdjp9IKFm" },
-    { date: new Date("2025-05"), src: "https://www.youtube.com/embed/zfbSivZ_u5Q?si=E6f_tT2rImdNz_BZ" },
-    { date: new Date("2025-06"), src: "https://www.youtube.com/embed/-V3wYZhvFFc?si=vf0d2GJXKFaedFIQ" },
-    { date: new Date("2025-07"), src: "https://www.youtube.com/embed/Ud6et5IuZoM?si=UVE1j_zdaVTO-AsR" },
-    { date: new Date("2025-08"), src: "https://www.youtube.com/embed/tZ-v_yiVtxE?si=I0Xkfe4-Ywgjvf-s" },
-    { date: new Date("2025-09"), src: "https://www.youtube.com/embed/rbUynoE2LeY?si=9XECwDBEc5fCaLS-" }
+  {
+    date: new Date("2025-01"),
+    src: "https://www.youtube.com/embed/bKIL70iP5Nc?si=Xc0D-TvtZ0Dx_WU3",
+  },
+  {
+    date: new Date("2025-02"),
+    src: "https://www.youtube.com/embed/lggIn-1yd8Q?si=fmNzsKmeaKYTCAxQ",
+  },
+  {
+    date: new Date("2025-03"),
+    src: "https://www.youtube.com/embed/1tHzphoNXXM?si=Qzx7iRUMz7pKr0_-",
+  },
+  {
+    date: new Date("2025-04"),
+    src: "https://www.youtube.com/embed/sTchSm6BqQQ?si=FoASemMCdjp9IKFm",
+  },
+  {
+    date: new Date("2025-05"),
+    src: "https://www.youtube.com/embed/zfbSivZ_u5Q?si=E6f_tT2rImdNz_BZ",
+  },
+  {
+    date: new Date("2025-06"),
+    src: "https://www.youtube.com/embed/-V3wYZhvFFc?si=vf0d2GJXKFaedFIQ",
+  },
+  {
+    date: new Date("2025-07"),
+    src: "https://www.youtube.com/embed/Ud6et5IuZoM?si=UVE1j_zdaVTO-AsR",
+  },
+  {
+    date: new Date("2025-08"),
+    src: "https://www.youtube.com/embed/tZ-v_yiVtxE?si=I0Xkfe4-Ywgjvf-s",
+  },
+  {
+    date: new Date("2025-09"),
+    src: "https://www.youtube.com/embed/rbUynoE2LeY?si=9XECwDBEc5fCaLS-",
+  },
+  {
+    date: new Date("2025-10"),
+    src: "https://www.youtube.com/embed/jbWW5ytoSps?si=gu6MWTobk3FEDXLA",
+  },
 ];
 
 videoDiaryEntries.forEach(render);
